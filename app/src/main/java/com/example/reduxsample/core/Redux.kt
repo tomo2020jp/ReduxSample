2e41d4b80f28c8580c71556d1f684e6d80ef37f2
tomo2020jp <39954318+tomo2020jp@users.noreply.github.com>
2022-08-07 08:30:53 +0900

    add redux core

---
+++ app/src/main/java/com/example/reduxsample/core/Redux.kt
@@ -0,0 +1,24 @@
+package com.example.reduxsample.core
+
+import android.database.Observable
+import io.reactivex.Single
+
+interface StateType
+
+interface ActionType
+
+interface ReducerType<STATE : StateType, ACTION : ActionType> {
+    fun reduce(state: STATE, action: ACTION): STATE
+}
+
+interface StoreType<STATE : StateType> {
+    fun dispatch(action: ActionType)
+    fun observable(): Observable<out STATE>
+    fun addMiddleware(middleware: MiddlewareType)
+    fun removeMiddleware(middleware: MiddlewareType)
+}
+
+interface MiddlewareType {
+    fun before(state: StateType, action: ActionType): Single<ActionType>
+    fun after(state: StateType, action: ActionType): Single<ActionType>
+}
